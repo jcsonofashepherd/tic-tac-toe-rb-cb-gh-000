0550407d5afd7f6667adfdec806a07c1a9b85924
John Casabar <johncasabar@gmail.com>
2018-10-12 04:48:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -87,8 +87,7 @@ def winner(board)
 end
 
 def play(board)
-  until over?(board) {turn(board)}
+  until over?(board) do turn(board) end
   if won?(board) {puts "Congratulations #{winner(board)}!"}
   if draw?(board) {puts "Cat's Game!"}
-  end
 end
