8df5de2779818e1a97c2e042c849103725af891d
John Casabar <johncasabar@gmail.com>
2018-10-12 04:21:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,4 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+# Helper Methods
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,34 +7,39 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
-def input_to_index(input)
-  idx = input.to_i
-  idx = idx - 1
+def input_to_index(user_input)
+  user_input.to_i - 1
 end
 
-def move(arr, idx, val = "X")
-  arr[idx] = val
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
 
-# code your #valid_move? method here
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
 def valid_move?(board, index)
-  if index > board.size - 1
-    false
-  elsif !position_taken?(board, index)
-    true
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
   else
-    false
+    turn(board)
   end
 end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board, index)
-  if board == []
-    false
-  elsif board[index] == "X" || board[index] == "O"
-    true
-  else
-    false
+# Define your play method below
+def play(board)
+  turns = 0
+  until turns == 9
+    turn(board)
+    turns += 1
   end
 end
